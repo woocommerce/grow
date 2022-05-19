4ad5928a6d68e18e0c38a8efeb519ad3d1116dc7
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-05-19 16:45:56 +0400

    Update package JSON

--- storybook/bin/storybook.js
+++ storybook/bin/storybook.js
@@ -4,15 +4,5 @@
  * Internal dependencies
  */
 const { shell } = require( '../utils' );
-const argv = require('process').argv;
 
-switch (argv[argv.length - 1]) {
-	case 'build':
-		shell.exec("BABEL_ENV=development build-storybook -c ./storybook -o ./storybook/dist");
-		break;
-	case 'deploy':
-		shell.exec("npm run storybook build && gh-pages -d ./storybook/dist");
-		break;
-	default:
-		shell.exec("start-storybook  -c ./storybook -p 6006 --ci");
-}
+shell.exec("start-storybook  -c ./storybook -p 6006 --ci");
