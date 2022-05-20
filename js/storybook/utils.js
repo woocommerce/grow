7b175a7378137c30d48855eae8245e69b3d78148
Miguel Perez Pellicer <5908855+puntope@users.noreply.github.com>
2022-05-20 12:12:42 +0400

    Remove JS folder

--- js/storybook/utils.js
+++
@@ -1,9 +0,0 @@
-/**
- * External dependencies
- */
-import shell from 'shelljs';
-import path from 'path';
-
-process.env.PATH += ( path.delimiter + path.join( process.cwd(), 'node_modules', '.bin') );
-
-export { shell };
