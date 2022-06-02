99141963a638199522a213fdda065f05b7847a9a
Tomek Wytrębowicz <tomalecpub@gmail.com>
2022-06-02 19:59:04 +0200

    Remove shared `utils.js`,
    as npm fails to resolve shelljs dependency.

--- packages/js/jsdoc/bin/jsdoc.js
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env node
-/**
- * Internal dependencies
- */
- import { shell } from '../../utils.js'
-
- shell.exec("jsdoc -r ./js/src -c .jsdocrc.json -t woocommerce-grow-tracking-jsdoc");
