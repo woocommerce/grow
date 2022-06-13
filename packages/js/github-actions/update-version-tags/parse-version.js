8bc4bc900e87462d0dcdd70b9827424210f7aca0
Eason Su <eason.su.tw@gmail.com>
2022-06-13 17:59:33 +0800

    Move the position of tool name in the version pattern to prefix for parseVersion function.
    Address: https://github.com/woocommerce/grow/pull/12#issuecomment-1152394922

--- packages/js/github-actions/update-version-tags/parse-version.js
+++ packages/js/github-actions/update-version-tags/parse-version.js
@@ -5,41 +5,49 @@ import semver from 'semver';
 
 /**
  * Parses a version name and returns its major and minor version names.
+ * The pattern of version name is `{prefix}-{version}`.
+ * - The format of `{prefix}` should be lowercase alphanumeric and hyphen characters,
+ *   but not leading with numbers and not continuous hyphens.
+ * - The format of `{version}` should be valid semantic versioning (https://semver.org/),
+ *   and the only exception is there can be a leading 'v' in `{version}`.
  *
- * The version format should be a valid semantic versioning. https://semver.org/
- * The only exception is that there can be a leading 'v'.
+ * For example:
+ * - tools-1.2.3
+ * - tools-v1.2.3
+ * - tools-v1.2.3-pre
+ * - ipv6-tools-v1.2.3
+ * - ipv6-tools-v1.2.3-beta.0
  *
- * @param {string} version The version to be parsed. For example, 'v1.2.3-tools'.
+ * @param {string} version The version to be parsed. For example, 'tools-v1.2.3'.
  * @return {{majorVersion: string, minorVersion: string} | null}
  *   The payload of major and minor version names.
- *   For example, { majorVersion: 'v1-tools', minorVersion: 'v1.2-tools' }
- *   `null` if the passed-in value is invalid semantic versioning.
+ *   For example, { majorVersion: 'tools-v1', minorVersion: 'tools-v1.2' }
+ *   `null` if the passed-in value doesn't match the pattern pattern.
  */
 export default function parseVersion( version ) {
-	// A leading 'v' is stripped off.
-	const validVersion = semver.valid( version );
+	const versionRegex = /^([a-z][a-z\d]*(?:-[a-z\d]+)*-)?(v)?(.+)$/;
+	const matched = version.match( versionRegex ) || [];
+	const [ , prefix, leadingV, semVersion ] = matched;
 
-	if ( validVersion === null ) {
+	if ( semver.valid( semVersion ) === null ) {
 		return null;
 	}
 
-	const { major, minor, prerelease } = semver.parse( validVersion );
-	const numberParts = [ major ];
-	const parts = [ numberParts ];
+	const { major, minor, prerelease } = semver.parse( semVersion );
+	const numberParts = [];
 
-	if ( validVersion !== version ) {
-		parts.unshift( 'v' );
-	}
-
-	if ( prerelease.length ) {
+	const parts = [
+		prefix,
+		leadingV,
+		numberParts,
 		// The `prerelease` could be ['beta', '1'], so adds them as 'beta.1' with a leading `-`.
-		parts.push( '-', prerelease.join( '.' ) );
-	}
+		prerelease.length && [ '-', prerelease.join( '.' ) ],
+	].filter( Boolean );
 
+	numberParts.push( major );
 	const majorVersion = parts.flat().join( '' );
 
 	numberParts.push( '.', minor );
-
 	const minorVersion = parts.flat().join( '' );
 
 	return {
