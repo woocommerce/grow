8da047b8e69a0036f29ea2aa1e0385bd40254431
Jeremy Pry <jeremy.pry@gmail.com>
2023-03-13 17:58:15 -0400

    Add placeholder files for action and documentor

---
+++ packages/actions/hook-documentation/Documentor.php
@@ -0,0 +1,14 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\Grow\Actions\HookDocumentation;
+
+/**
+ * Class Documentor
+ *
+ * @since x.x.x
+ */
+class Documentor {
+
+}
