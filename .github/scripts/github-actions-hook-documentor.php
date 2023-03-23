fd6f48b589e124e74d85997d5b1a951d51b59811
Jeremy Pry <jeremy.pry@gmail.com>
2023-03-23 18:36:20 -0400

    Partial updates to script and action

--- .github/scripts/github-actions-hook-documentor.php
+++ .github/scripts/github-actions-hook-documentor.php
@@ -11,13 +11,27 @@ if ( ! file_exists( $autoloadPath ) ) {
 
 require_once $autoloadPath;
 
+// Source directories need the full path prepended.
+$source_dirs = array_map(
+	function( $path ) {
+		$path = trim( $path );
+
+		return $path;
+	},
+	explode( ',', $_ENV['INPUT_SOURCE-DIRECTORIES'] ?? 'src/' )
+);
+
+$args = [
+	'output_file' => $_ENV['INPUT_OUTPUT-FILE'] ?? 'docs/Hooks.md',
+];
+
 $documentor = new Documentor(
 	[
-		'github_path'  => '',
+		'github_path' => '',
 		'source_dirs' => [
 			'src/',
 		],
-		'output_file'  => $_ENV['INPUT_OUTPUT-FILE'] ?? 'docs/Hooks.md',
+		'output_file' => $_ENV['INPUT_OUTPUT-FILE'] ?? 'docs/Hooks.md',
 	]
 );
 
