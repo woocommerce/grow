2e708f22ff31e9dad2c8fd027d5482d3e447e684
Jeremy Pry <jeremy.pry@gmail.com>
2023-05-03 13:19:52 -0400

    Update tests to span 2 files

---
+++ packages/php/github-actions/hook-documentation/tests/Unit/DocumentorContentTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\Grow\GitHubActions\HookDocumentation\Tests;
+
+use Automattic\WooCommerce\Grow\GitHubActions\HookDocumentation\Documentor;
+
+it(
+	'should find actions in sample file 1',
+	function() {
+		$workspace   = dirname( __DIR__ );
+		$github_repo = 'https://github.com/example/test';
+		$sha         = 'abc123';
+		$args        = [
+			'github_path' => $github_repo,
+			'github_blob' => $sha,
+			'workspace'   => $workspace,
+			'source_dirs' => [ 'Data/' ],
+		];
+
+		$documentor = new Documentor( $args );
+		$results    = $documentor->generate_hooks_docs();
+		expect( $results )->not()->toBeEmpty()->toBeString();
+
+		return $results;
+	}
+);
+
+it(
+	'should find all actions in sample file 1',
+	function( string $results ) {
+		expect( $results )->toContain( 'sample_action_1', 'sample_action_2' );
+	}
+)->depends( 'it should find actions in sample file 1' );
+
+it(
+	'should find sample action 1 only once',
+	function( string $results ) {
+		expect( $results )->toContainCount( 'sample_action_1', 1 );
+	}
+)->depends( 'it should find actions in sample file 1' );
+
+it(
+	'should find the sample-file-1 filename 6 times – twice for each action',
+	function( string $results ) {
+		expect( $results )->toContainCount( 'sample-file-1.php', 6 );
+	}
+)->depends( 'it should find actions in sample file 1' );
+
+it(
+	'should not contain the full workspace directory',
+	function( string $results ) {
+		expect( $results )->not()->toContain( dirname( __DIR__ ) . '/Data' );
+	}
+)->depends( 'it should find actions in sample file 1' );
--- packages/php/github-actions/hook-documentation/tests/Unit/DocumentorTest.php
+++ packages/php/github-actions/hook-documentation/tests/Unit/DocumentorTest.php
@@ -8,6 +8,8 @@ use Automattic\WooCommerce\Grow\GitHubActions\HookDocumentation\Documentor;
 use Closure;
 use RuntimeException;
 
+use function Automattic\WooCommerce\Grow\GitHubActions\HookDocumentation\Tests\getTestDataDocumentor;
+
 it(
 	'should throw an exception for missing constructor args',
 	function( array $args, string $message ) {
@@ -18,6 +20,7 @@ it(
 		)->toThrow( RuntimeException::class, $message );
 	}
 )->with( 'constructors' );
+/** @see packages/php/github-actions/hook-documentation/tests/Datasets/Constructors.php */
 
 it(
 	'should correctly replace local path with github url',
@@ -65,50 +68,21 @@ it(
 );
 
 it(
-	'should find actions in sample file 1',
+	'should gracefully handle a non-existent directory',
 	function() {
-		$workspace   = dirname( __DIR__ );
-		$github_repo = 'https://github.com/example/test';
-		$sha         = 'abc123';
-		$args        = [
-			'github_path' => $github_repo,
-			'github_blob' => $sha,
-			'workspace'   => $workspace,
-			'source_dirs' => [ 'Data/' ],
-		];
-
-		$documentor = new Documentor( $args );
-		$results    = $documentor->generate_hooks_docs();
-		expect( $results )->not()->toBeEmpty()->toBeString();
-
-		return $results;
+		expect(
+			getTestDataDocumentor( 'Data/', 'FakeDir/' )->generate_hooks_docs()
+		)->not()->toBeEmpty()->toBeString();
 	}
 );
 
 it(
-	'should find all actions in sample file 1',
-	function( string $results ) {
-		expect( $results )->toContain( 'sample_action_1', 'sample_action_2' );
+	'should throw an exception when no hooks were found',
+	function() {
+		expect(
+			function() {
+				getTestDataDocumentor( 'FakeDir/' )->generate_hooks_docs();
+			}
+		)->toThrow( RuntimeException::class, 'No hooks found' );
 	}
-)->depends( 'it should find actions in sample file 1' );
-
-it(
-	'should find sample action 1 only once',
-	function( string $results ) {
-		expect( $results )->toContainCount( 'sample_action_1', 1 );
-	}
-)->depends( 'it should find actions in sample file 1' );
-
-it(
-	'should find the sample-file-1 filename 6 times – twice for each action',
-	function( string $results ) {
-		expect( $results )->toContainCount( 'sample-file-1.php', 6 );
-	}
-)->depends( 'it should find actions in sample file 1' );
-
-it(
-	'should not contain the full workspace directory',
-	function( string $results ) {
-		expect( $results )->not()->toContain( dirname( __DIR__ ) . '/Data' );
-	}
-)->depends( 'it should find actions in sample file 1' );
+);
