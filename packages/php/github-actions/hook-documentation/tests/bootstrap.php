f9fc4b56f01957741c5ef1449e01395a889954d9
Jeremy Pry <jeremy.pry@gmail.com>
2023-05-02 23:24:13 -0400

    Add noop file for simple function recognition

--- packages/php/github-actions/hook-documentation/tests/bootstrap.php
+++ packages/php/github-actions/hook-documentation/tests/bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+declare( strict_types=1 );
+
+namespace Automattic\WooCommerce\Grow\GitHubActions\HookDocumentation\Tests;
 
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
-
+require_once __DIR__ . '/noop.php';
