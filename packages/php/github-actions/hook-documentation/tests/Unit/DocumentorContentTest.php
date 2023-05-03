68337440db9827b1e9c0d3acbf9626f774689d4b
Jeremy Pry <jeremy.pry@gmail.com>
2023-05-03 18:40:04 -0400

    Update tests to check for hooks from sample file 2

--- packages/php/github-actions/hook-documentation/tests/Unit/DocumentorContentTest.php
+++ packages/php/github-actions/hook-documentation/tests/Unit/DocumentorContentTest.php
@@ -7,7 +7,7 @@ namespace Automattic\WooCommerce\Grow\GitHubActions\HookDocumentation\Tests;
 use Automattic\WooCommerce\Grow\GitHubActions\HookDocumentation\Documentor;
 
 it(
-	'should find actions in sample file 1',
+	'should find actions in sample files',
 	function() {
 		$workspace   = dirname( __DIR__ );
 		$github_repo = 'https://github.com/example/test';
@@ -32,25 +32,39 @@ it(
 	function( string $results ) {
 		expect( $results )->toContain( 'sample_action_1', 'sample_action_2' );
 	}
-)->depends( 'it should find actions in sample file 1' );
+)->depends( 'it should find actions in sample files' );
 
 it(
 	'should find sample action 1 only once',
 	function( string $results ) {
 		expect( $results )->toContainCount( 'sample_action_1', 1 );
 	}
-)->depends( 'it should find actions in sample file 1' );
+)->depends( 'it should find actions in sample files' );
 
 it(
 	'should find the sample-file-1 filename 6 times – twice for each action',
 	function( string $results ) {
 		expect( $results )->toContainCount( 'sample-file-1.php', 6 );
 	}
-)->depends( 'it should find actions in sample file 1' );
+)->depends( 'it should find actions in sample files' );
 
 it(
 	'should not contain the full workspace directory',
 	function( string $results ) {
 		expect( $results )->not()->toContain( dirname( __DIR__ ) . '/Data' );
 	}
-)->depends( 'it should find actions in sample file 1' );
+)->depends( 'it should find actions in sample files' );
+
+it(
+	'should find all actions in sample file 2',
+	function( string $results ) {
+		expect( $results )->toContain(
+			'class_action_1',
+			'class_action_2',
+			'class_action_ref_array',
+			'class_filter_1',
+			'class_magic_action',
+			'class_magic_action_$NAME'
+		);
+	}
+)->depends( 'it should find actions in sample files' );
