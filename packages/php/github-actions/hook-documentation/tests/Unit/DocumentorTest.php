573271d0c4403a931f1be099f74100fe17772769
Jeremy Pry <jeremy.pry@gmail.com>
2023-05-03 00:00:40 -0400

    Add tests that depend on the same result
    Doing it this way makes it easier to identify the various conditions that are being tested.

--- packages/php/github-actions/hook-documentation/tests/Unit/DocumentorTest.php
+++ packages/php/github-actions/hook-documentation/tests/Unit/DocumentorTest.php
@@ -79,6 +79,36 @@ it(
 
 		$documentor = new Documentor( $args );
 		$results    = $documentor->generate_hooks_docs();
-		expect( $results )->not->toBeEmpty();
+		expect( $results )->not()->toBeEmpty()->toBeString();
+
+		return $results;
 	}
 );
+
+it(
+	'should find all actions in sample file 1',
+	function( string $results ) {
+		expect( $results )->toContain( 'sample_action_1', 'sample_action_2' );
+	}
+)->depends( 'it should find actions in sample file 1' );
+
+it(
+	'should find sample action 1 only once',
+	function( string $results ) {
+		expect( $results )->toContainCount( 'sample_action_1', 1 );
+	}
+)->depends( 'it should find actions in sample file 1' );
+
+it(
+	'should find the sample-file-1 filename 6 times – twice for each action',
+	function( string $results ) {
+		expect( $results )->toContainCount( 'sample-file-1.php', 6 );
+	}
+)->depends( 'it should find actions in sample file 1' );
+
+it(
+	'should not contain the full workspace directory',
+	function( string $results ) {
+		expect( $results )->not()->toContain( dirname( __DIR__ ) . '/Data' );
+	}
+)->depends( 'it should find actions in sample file 1' );
